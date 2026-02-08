e4043ab8325619451be5f984ab0e962206cf1b48
Wouter Heyvaert <wouterheyvaert1@gmail.com>
2026-02-08 17:25:16 +0100

    feat(infra): add provider and backend

---
+++ infra/terraform.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>6.0"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "bestsurfspot-terraform-state"
+    key          = "terraform.tfstate"
+    region       = "eu-west-1"
+    use_lockfile = true
+    encrypt      = true
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+
+  default_tags {
+    tags = {
+      app = var.app_name
+      env = var.env
+    }
+  }
+}
---
+++ infra/variables.tf
@@ -0,0 +1,8 @@
+variable "app_name" {
+  type    = string
+  default = "bestsurfspot"
+}
+
+variable "env" {
+  type = string
+}
